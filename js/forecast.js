fb701c866f77a2c6ecf22f585882cdd6f3d675d5
ulugbekhusainov <husainov078@gmail.com>
2023-02-20 20:05:00 +0500

    weather

---
+++ js/forecast.js
@@ -0,0 +1,16 @@
+const KEY = "96b947a45d33d7dc1c49af3203966408";
+// bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
+// Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
+// Aloqa: Telegram => @akror_web
+
+const getData = async (city) => {
+  const base = "https://api.openWeathermap.org/data/2.5/weather";
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
+  console.log("Loading...");
+  loader(true);
+  const req = await fetch(base + query);
+  const data = await req.json();
+  loader(false);
+  console.log("Finished!!");
+  return data;
+};
